652864b09204b9105f2e4b6db4835e954e57bc96
Hevanafa <dhevansafa4@gmail.com>
2026-01-14 06:49:09 +0700

    added experimental boilerplate

---
+++ experimental/game.cpp
@@ -0,0 +1,28 @@
+#include <emscripten.h>
+#include <stdint.h>
+
+#include "posit_92.hpp"
+#include "includes\vga.h"
+
+extern "C" {
+  void signalDone();
+  void vgaFlush();
+}
+
+export void cleanup() {
+
+}
+
+export void init() {
+  initBuffer();
+}
+
+export void update() {
+
+}
+
+export void draw() {
+  cls(0xFF6495ED);
+
+  vgaFlush();
+}
---
+++ experimental/posit_92.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define export extern "C" EMSCRIPTEN_KEEPALIVE
+
+#define PByte uint8_t*
+
+#define int64 long
+#define longword unsigned int
+#define smallint short
+#define word unsigned short
+#define byte unsigned char
