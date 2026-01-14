7ae068bd83459495430364bf1d042910aa4b739c
Hevanafa <dhevansafa4@gmail.com>
2026-01-14 11:46:43 +0700

    added vline implementation

--- unstable/includes/graphics.hpp
+++ unstable/includes/graphics.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "pascal_compat.hpp"
+#include "vga.hpp"
 
 byte getAlpha(const longword colour) {
-  return colour >> 24 & 0xFF
+  return colour >> 24 & 0xFF;
 }
 
 longword solidify(const longword colour) {
@@ -13,11 +14,26 @@ longword solidify(const longword colour) {
 }
 
 void vline(
-  smallint x0, smallint x1, smallint y,
+  smallint x, smallint y0, smallint y1,
   const longword colour)
 {
-  smallint x;
-  longword solid;
+  smallint y, temp;
+
+  if (getAlpha(colour) == 0) return;
+  if ((x < 0) || (x >= vgaWidth)) return;
+
+  longword solid = solidify(colour);
+
+  if (y1 < y0) {
+    temp = y0; y0 = y1; y1 = temp;
+  }
+
+  if (y0 < 0) y0 = 0;
+  if (y1 >= vgaHeight) y1 = vgaHeight - 1;
+  if (y0 > y1) return;
+
+  for (y = y0; y0 <= y1; y++)
+    unsafePset(x, y, solid);
 }
 
 void line(smallint x0, smallint y0, smallint x1, smallint y1, longword colour) {
