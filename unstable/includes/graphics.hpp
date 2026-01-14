725f7fe386be50a34bbababe2d07e7c6e22b47a1
Hevanafa <dhevansafa4@gmail.com>
2026-01-14 11:41:54 +0700

    added solidify & get alpha

--- unstable/includes/graphics.hpp
+++ unstable/includes/graphics.hpp
@@ -2,12 +2,30 @@
 
 #include "pascal_compat.hpp"
 
+byte getAlpha(const longword colour) {
+  return colour >> 24 & 0xFF
+}
+
+longword solidify(const longword colour) {
+  return (colour & 0xFF000000 > 0)
+    ? colour | 0xFF000000
+    : colour;
+}
+
+void vline(
+  smallint x0, smallint x1, smallint y,
+  const longword colour)
+{
+  smallint x;
+  longword solid;
+}
+
 void line(smallint x0, smallint y0, smallint x1, smallint y1, longword colour) {
   smallint dx, dy, sx, sy, err, e2;
   longword solid;
 
-  if (colour & 0xFF000000 == 0) return;
-  solid = colour | 0xFF000000;
+  if (getAlpha(colour) == 0) return;
+  solid = solidify(colour);
 
   if (x0 == x1) {
     vline(x0, y0, y1, solid);
@@ -15,7 +33,7 @@ void line(smallint x0, smallint y0, smallint x1, smallint y1, longword colour) {
   }
 
   if (y0 == y1) {
-    hline(x0, x1, y0, solid);
+    // hline(x0, x1, y0, solid);
     return;
   }
 
