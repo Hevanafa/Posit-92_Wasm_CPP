312dc255ed9719a53d4f9c80e487330c65d60fa3
Hevanafa <dhevansafa4@gmail.com>
2026-01-17 11:18:53 +0700

    added f32str

--- experimental/includes/conv.hpp
+++ experimental/includes/conv.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 #include <string>
+#include <sstream>  // ostringstream, fixed
+#include <iomanip>  // setprecision
+
 #include "pascal_compat.hpp"
 
 std::string i32str(const LongInt value) {
   return std::to_string(value);
+}
+
+std::string f32str(const double value) {
+  std::ostringstream oss;
+  oss << std::fixed << std::setprecision(4) << value;
+  return oss.str();
 }
